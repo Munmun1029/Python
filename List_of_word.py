bbdda5bfa3895de7c95fbb99f21197751d7ec779
Farhana Yeasmin Munmun <106430682+Munmun1029@users.noreply.github.com>
2023-12-07 00:36:16 +0600

    Add files via upload

---
+++ List_of_word.py
@@ -0,0 +1,6 @@
+word_list = ["i Love You", "Miss You",
+             'love', 'miss', 'hate',
+             'soulmate', 'heart mate',
+             'best friend', 'bff', "Sleep",
+             "Career", "Fruit", "Potato", "Kodu",
+             "Shopping", "Phone", "Watch"]
