beb4d59d6c598b502289258b99d6a82796db0ec3
Farhana Yeasmin Munmun <106430682+Munmun1029@users.noreply.github.com>
2023-12-06 23:36:45 +0600

    Add files via upload

---
+++ For_loop.py
@@ -0,0 +1,11 @@
+names = ['farhana ', 'yeasmin', 'munmun', 'mahi']
+
+for name in names:
+    print(name)
+
+number = [3, 5, 6, 7, 8, 10]
+squares = []
+for num in number:
+    square = num ** 2
+    squares.append(square)
+print("The list of squares is :", squares)
